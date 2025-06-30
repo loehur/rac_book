429abcfba9183bc938289ff2d625016e311ef129
loehur <loehur@gmail.com>
2025-06-30 16:11:30 +0700

    logo3444444343343343333334433443433

--- app/Views/Search/data.php
+++ app/Views/Search/data.php
@@ -1,17 +1,27 @@
 <?php
 $d = $data;
-$hp = $this->data('Validasi')->valid_wa_direct($d['hp']);
-?>
-<div class="p-2 rounded border">
-    <div class="d-flex justify-content-between">
-        <div class="px-1">
-            <span class="fw-bold"><?= $d['nama'] ?></span><br>
-            <?= $hp ?>
-        </div>
-        <div style="cursor: pointer;" class="px-1 text-success">
-            <a target="_blank" class="text-success" href="https://api.whatsapp.com/send?phone=<?= $hp ?>&text=">
-                <h2><i class="fab fa-whatsapp"></i></h2>
-            </a>
+if (isset($d['hp'])) {
+    $hp = $this->data('Validasi')->valid_wa_direct($d['hp']); ?>
+    <div class="p-2 rounded border">
+        <div class="d-flex justify-content-between">
+            <div class="px-1">
+                <span class="fw-bold"><?= $d['nama'] ?></span><br>
+                <?= $hp ?>
+            </div>
+            <div style="cursor: pointer;" class="px-1 text-success">
+                <a target="_blank" class="text-success" href="https://api.whatsapp.com/send?phone=<?= $hp ?>&text=">
+                    <h2><i class="fab fa-whatsapp"></i></h2>
+                </a>
+            </div>
         </div>
     </div>
-</div>
+<?php
+} else { ?>
+    <div class="p-2 rounded border">
+        <div class="d-flex justify-content-between">
+            <div class="px-1">
+                No Data
+            </div>
+        </div>
+    </div>
+<?php } ?>
