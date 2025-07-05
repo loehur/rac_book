76fa8853e5209ec38b00f03d2188339fa1af15d8
loehur <loehur@gmail.com>
2025-07-05 08:51:12 +0700

    tes445

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -11,7 +11,7 @@ class User extends Controller
    public function index()
    {
       $layout = ['title' => "User"];
-      $where = "en = 1";
+      $where = "en = 1 AND id_user <> 24";
       $data = $this->db(0)->get_where("user", $where);
       $this->view('layout', $layout);
       $this->view(__CLASS__ . "/main", $data);
