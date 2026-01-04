16d7d0905e16fd6f33f9ef8b6db9c39eda13d2f9
loehur <loehur@gmail.com>
2026-01-04 13:56:31 +0700

    by pass dfd656566

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-$_start_time = microtime(true);
-
 require_once('app/init.php');
-
-$_init_time = microtime(true);
-
 $route = new Route();
-
-$_end_time = microtime(true);
-
-// Debug timing (hapus setelah selesai debug)
-echo "<!-- DEBUG TIMING:
-  Init: " . round(($_init_time - $_start_time) * 1000, 2) . "ms
-  Route: " . round(($_end_time - $_init_time) * 1000, 2) . "ms
-  Total: " . round(($_end_time - $_start_time) * 1000, 2) . "ms
--->";
